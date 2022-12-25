0ebe8b885f1e8e64b73767ad804be1ff33720ad2
Zarina523 <57904067+Zarina523@users.noreply.github.com>
2022-12-25 21:54:58 +0300

    Update GenAlg.py

--- GenAlg.py
+++ GenAlg.py
@@ -1,118 +1,85 @@
 import random
 
+import numpy
+import numpy as np
 
-class GADiophant:
-    survive_coef: int = 0.3
-    productivity: int = 4
-
-    def __init__(
-        self,
-        coefficients: list[int],
-        answer: int,
-        bounds=None,
-        steps=100,
-        stop_fitness=None,
-        population_limit=25,
-    ):
-        self.coefficients = coefficients
-        self.answer = answer
-        self.steps = steps
-        self.stop_fitness = stop_fitness
-        self.population_limit = population_limit
-        self.bounds = [(bounds[0], bounds[1], bounds[2])] * len(self.coefficients)
-        self.best = []
-
-    def evolve(self):
-        newborns = []
-        best = None
-        for _ in range(self.steps):
-            population = self.__generate_population(newborns)
-            survivors = sorted(population, key=lambda i: -i[1])[: int(self.population_limit * self.survive_coef)]
-            newborns = self.__cross(survivors)
-
-            self.best.append(survivors[0])
-            if not best:
-                best = self.best[-1]
-            else:
-                best = max(best, self.best[-1], key=lambda i: i[1])
-
-            if self.stop_fitness != None and best[-1] >= self.stop_fitness:
-                break
-
-        return best
-
-    def __diophant(self, individs):
-        ans = self.answer
-        for i, coef in enumerate(self.coefficients):
-            ans -= coef * individs[i]
-        return -abs(ans)
-
-    def __generate_population(self, newborns):
-        population = []
-        for individ in newborns:
-            individ = self.__mutation(individ)
-            fitness = self.__diophant(individ)
-            population.append((individ, fitness))
-
-        for _ in range(self.population_limit - len(newborns)):
-            individs = []
-            for bound in self.bounds:
-                step = bound[2]
-                gene = random.choice(self.__frange(bound[0], bound[1] + step, step))
-                individs.append(gene)
-
-            fitness = self.__diophant(individs)
-            population.append((individs, fitness))
-            newborns.append(individs)
-        return population
-
-    def __cross(self, best):
-        newborns = []
-        for _ in range(len(best) * self.productivity):
-            dad, mom = random.sample(best, 2)
-            dad, mom = dad[0], mom[0]
-            split = len(dad) // 2
-            child = dad[:split] + mom[split:]
-            newborns.append(child)
-        return newborns
-
-    def __mutation(self, indiv):
-        gene_id = random.randint(0, len(indiv) - 1)
-        step = self.bounds[gene_id][2]
-        step = random.choice([-step, step])
-        while not (self.bounds[gene_id][0] <= indiv[gene_id] + step <= self.bounds[gene_id][1]):
-            step = self.bounds[gene_id][2]
-            step = random.choice([-step, step])
-
-        indiv[gene_id] += step
-        return indiv
-
-    @staticmethod
-    def __frange(start, stop, step):
-        flist = []
-        while start < stop:
-            flist.append(start)
-            start += step
-        return flist
+EXPERIMENT = [5, -1, 6, 7, -9, -4]
+Y = 60
+weights = len(EXPERIMENT)
+C = 6
+SIZE = (C, weights)
+POP = np.random.randint(low=-len(EXPERIMENT), high=len(EXPERIMENT), size=SIZE)
+ITERATION = 3000
 
 
+def fit(population, y):
+    pop = []
+    for i in range(len(population)):
+        s = 0
+        for j in range(len(EXPERIMENT)):
+            s += population[i][j] * EXPERIMENT[j]
+        r = np.abs(y - s) + 1
+        pop.append(1 / r)
+    return pop
 
+
+def result():
+    for iteration in range(ITERATION):
+        fitness = fit(POP, Y)
+        new_parents = selection(POP, 6, fitness)
+        new_c_co = co(parents=new_parents, size=(SIZE[0] - len(new_parents), weights))
+        new_off_mut = mut(new_c_co, 0.1)
+        for i in range(len(new_parents)):
+            POP[i] = new_parents[i]
+        count = 0
+        for i in range(len(new_parents), len(new_parents) + len(new_off_mut)):
+            POP[i] = new_off_mut[count]
+            count += 1
+    fitness = fit(POP, Y)
+    max_f = max(fitness)
+    index = fitness.index(max_f)
+    print("The best chromosome: ", POP[index])
+    index_best = numpy.where(fitness == numpy.max(fitness))
+    print(fitness)
+    print(index_best)
+    print(POP)
+    print(POP[index_best, :])
+
+
+def selection(pop, number, p):
+    parents = []
+    for i in range(number):
+        max_ind = [j for j in range(len(p)) if p[j] == max(p)][0]
+        parents.append(pop[max_ind])
+        p.remove(max(p))
+    return parents
+
+
+def co(parents, size):
+    off = np.empty(size)
+    point = np.uint8(size[1] / 2)
+    for k in range(size[0]):
+        p1 = k % len(parents)
+        p2 = (k + 1) % len(parents)
+        for i in range(point):
+            off[k][i] = parents[p1][i]
+
+        for i in range(point, size[1]):
+            off[k][i] = parents[p2][i]
+    return off
+
+
+def mut(after_c, mutation):
+    p = []
+    for i in range(0, len(after_c)):
+        chrom = after_c[i]
+        for j in range(len(chrom)):
+            if random.random() < mutation:
+                random_value = np.random.randint(-1.0, 1.0, 1)
+                chrom[j] = chrom[j] + random_value
+        p.append(chrom)
+    return p
 
 
 if __name__ == '__main__':
-    ga = GADiophant(
-        coefficients=(1, 2, 3, 4, 5, 6), # линейное уравнение вида: ax1 + bx2 + ... + zxn
-        answer=123456,
-        bounds=(-20000, 20000, 2),
-        steps=1000,
-        stop_fitness=0,
-        population_limit=100,
-    )
-    result = ga.evolve()
-
-    print_str = ''
-    for i, x in enumerate(result[0]):
-        print_str += f"{ga.coefficients[i]} * {x} + "
-    print_str = print_str.strip()[:-1] + f"= {result[1]}"
-
-    print("Solution:", print_str)
+    result()
